04a8d5c1d3015cd5498f40b19baedf22b6c5ad20
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-11-25 06:19:45 +0200

    intro

--- HelloXcode/ContentView.swift
+++ HelloXcode/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            Color
+                .purple
+                .opacity(0.5)
+                .ignoresSafeArea()
+            VStack {
+                Image(systemName: "figure.run")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Welcome to iOS Development")
+                    .foregroundStyle(.red)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.semibold)
+                Text("First App")
+                    .font(.title)
+                    .fontWeight(.thin)
+                    .padding()
+                    .background(
+                        Color.blue
+                            .opacity(0.8)
+                    )
+
+                Text("Coding is fun")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
